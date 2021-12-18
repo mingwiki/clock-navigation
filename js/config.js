6f8b1443ab51e476ef40e1b116cfe176319133f4
付明 <mingwiki@gmail.com>
2021-12-18 14:39:56 +0800

    change nav

--- js/config.js
+++ js/config.js
@@ -7,208 +7,92 @@ const CONFIG = {
    * Update line 11 and 13 if you prefer using Google.
    */
   commands: [{
-      name: 'Google',
-      key: '*',
-      url: 'https://google.com',
-      search: '/search?q={}'
+      name: "Google",
+      key: "*",
+      url: "https://google.com",
+      search: "/search?q={}",
     },
     {
-      category: 'General',
-      name: 'Mail',
-      key: 'm',
-      url: 'https://gmail.com',
-      search: '/#search/text={}',
-      color: 'linear-gradient(135deg, #dd5145, #dd5145)',
-      icon: 'mail',
+      category: "General",
+      name: "Mail",
+      key: "m",
+      url: "https://gmail.com",
+      search: "/#search/text={}",
+      color: "linear-gradient(135deg, #dd5145, #dd5145)",
+      icon: "mail",
+      quickLaunch: true,
+    },
+
+    {
+      category: "Programming",
+      name: "My Git",
+      key: "g",
+      url: "https://git.naizi.fun/mingwiki",
+      search: "/search?q={}",
+      color: "linear-gradient(135deg, #2b2b2b, #3b3b3b)",
+      icon: "github",
       quickLaunch: true,
     },
     {
-      category: 'General',
-      name: 'Drive',
-      key: 'd',
-      url: 'https://drive.google.com',
-      search: '/drive/search?q={}',
-      color: 'linear-gradient(135deg, #FFD04B, #1EA362, #4688F3)',
-      icon: 'drive',
-      quickLaunch: false,
+      category: "Programming",
+      name: "StackOverflow",
+      key: "st",
+      url: "https://stackoverflow.com",
+      search: "/search?q={}",
+      color: "linear-gradient(135deg, #53341C, #F48024)",
+      icon: "stackoverflow",
+      quickLaunch: true,
     },
     {
-      category: 'General',
-      name: 'Telegram',
-      key: 'tg',
-      url: 'https://web.telegram.org',
-      color: '#5682a3',
-      icon: 'telegram',
-      quickLaunch: false,
-    },
-    {
-      category: 'General',
-      name: 'WhatsApp',
-      key: 'w',
-      url: 'https://web.whatsapp.com',
-      color: 'linear-gradient(135deg, #25D366, #128C7E, #075E54)',
-      icon: 'whatsapp',
-      quickLaunch: false,
-    },
-    {
-      category: 'General',
-      name: 'Discord',
-      key: 'dc',
-      url: 'https://discord.com/app',
-      color: '#7289da',
-      icon: 'discord',
+      category: "Programming",
+      name: "MDN",
+      key: "md",
+      url: "https://developer.mozilla.org/zh-CN/",
+      search: "/search?q={}",
+      color: "#212121",
+      icon: "mdn",
       quickLaunch: false,
     },
 
-
     {
-      category: 'Programming',
-      name: 'GitHub',
-      key: 'g',
-      url: 'https://github.com',
-      search: '/search?q={}',
-      color: 'linear-gradient(135deg, #2b2b2b, #3b3b3b)',
-      icon: 'github',
-      quickLaunch: true,
-    },
-    {
-      category: 'Programming',
-      name: 'StackOverflow',
-      key: 'st',
-      url: 'https://stackoverflow.com',
-      search: '/search?q={}',
-      color: 'linear-gradient(135deg, #53341C, #F48024)',
-      icon: 'stackoverflow',
-      quickLaunch: true,
-    },
-    {
-      category: 'Programming',
-      name: 'HackerNews',
-      key: 'h',
-      url: 'https://news.ycombinator.com/',
-      search: '/search?stories[query]={}',
-      color: 'linear-gradient(135deg, #FF6600, #DC5901)',
-      icon: 'hackernews',
-      quickLaunch: true,
-    },
-    {
-      category: 'Programming',
-      name: 'MDN',
-      key: 'md',
-      url: 'https://developer.mozilla.org/en-US',
-      search: '/search?q={}',
-      color: '#212121',
-      icon: 'mdn',
+      category: "Fun",
+      name: "YouTube",
+      key: "y",
+      url: "https://youtube.com",
+      search: "/results?search_query={}",
+      color: "linear-gradient(135deg, #cd201f, #cd4c1f)",
+      icon: "youtube",
       quickLaunch: false,
     },
     {
-      category: 'Programming',
-      name: 'DevDocs',
-      key: 'dd',
-      url: 'https://devdocs.io',
-      color: 'linear-gradient(135deg, #33373A, #484949)',
-      icon: 'devdocs',
+      category: "Fun",
+      name: "YouTube Music",
+      key: "ym",
+      url: "https://music.youtube.com/",
+      search: "/search?q={}",
+      color: "linear-gradient(135deg, #cd201f, #cd4c1f)",
+      icon: "music",
       quickLaunch: false,
     },
 
-
     {
-      category: 'Fun',
-      name: 'YouTube',
-      key: 'y',
-      url: 'https://youtube.com',
-      search: '/results?search_query={}',
-      color: 'linear-gradient(135deg, #cd201f, #cd4c1f)',
-      icon: 'youtube',
+      category: "Other",
+      name: "Translate",
+      key: "tr",
+      url: "https://translate.google.com/",
+      search: "/#view=home&op=translate&sl=auto&tl=en&text={}",
+      color: "#1a73e8",
+      icon: "translate",
       quickLaunch: false,
     },
     {
-      category: 'Fun',
-      name: 'Reddit',
-      key: 'r',
-      url: 'https://reddit.com',
-      search: '/search?q={}',
-      color: 'linear-gradient(135deg, #FF8456, #FF4500)',
-      icon: 'reddit',
-      quickLaunch: false,
-    },
-    {
-      category: 'Fun',
-      name: 'Netflix',
-      key: 'n',
-      url: 'https://www.netflix.com',
-      color: 'linear-gradient(135deg, #E50914, #CB020C)',
-      icon: 'netflix',
-      quickLaunch: false,
-    },
-    {
-      category: 'Fun',
-      name: 'Spotify',
-      key: 's',
-      url: 'https://open.spotify.com/',
-      search: '/search/{}',
-      color: '#1dd35e',
-      icon: 'spotify',
-      quickLaunch: false,
-    },
-    {
-      category: 'Fun',
-      name: 'Twitch',
-      key: 'tw',
-      url: 'https://www.twitch.tv',
-      search: '/directory/game/{}',
-      color: 'linear-gradient(135deg, #6441a5, #4b367c)',
-      icon: 'twitch',
-      quickLaunch: false,
-    },
-
-
-    {
-      category: 'Other',
-      name: 'Twitter',
-      key: 't',
-      url: 'https://www.twitter.com',
-      search: '/search?q={}&src=typed_query',
-      color: 'linear-gradient(135deg, #1DA1F2, #19608F)',
-      icon: 'twitter',
-      quickLaunch: true,
-    },
-    {
-      category: 'Other',
-      name: 'Instagram',
-      key: 'i',
-      url: 'https://www.instagram.com',
-      color: 'linear-gradient(45deg, #405de6, #5851db, #833ab4, #c13584, #e1306c, #fd1d1d)',
-      icon: 'instagram',
-      quickLaunch: true,
-    },
-    {
-      category: 'Other',
-      name: 'LinkedIn',
-      key: 'l',
-      url: 'https://linkedin.com',
-      search: '/search/results/all/?keywords={}',
-      color: 'linear-gradient(135deg, #006CA4, #0077B5)',
-      icon: 'linkedin',
-      quickLaunch: true,
-    },
-    {
-      category: 'Other',
-      name: 'Notion',
-      key: 'ns',
-      url: 'https://www.notion.so',
-      color: 'linear-gradient(135deg, #FFF, #3F3F3F)',
-      icon: 'notion',
-      quickLaunch: true,
-    },
-    {
-      category: 'Other',
-      name: 'Translate',
-      key: 'tr',
-      url: 'https://translate.google.com/',
-      search: '/#view=home&op=translate&sl=auto&tl=en&text={}',
-      color: '#1a73e8',
-      icon: 'translate',
+      category: "Other",
+      name: "iconfont",
+      key: "if",
+      url: "https://www.iconfont.cn/",
+      search: "/search/index?searchType=icon&q={}",
+      color: "#1a73e8",
+      icon: "library-img",
       quickLaunch: false,
     },
   ],
@@ -229,20 +113,20 @@ const CONFIG = {
    * - "History" suggestions come from your previously entered queries
    */
   influencers: [{
-      name: 'Commands',
-      limit: 2
+      name: "Commands",
+      limit: 2,
     },
     {
-      name: 'Default',
-      limit: 4
+      name: "Default",
+      limit: 4,
     },
     {
-      name: 'History',
-      limit: 1
+      name: "History",
+      limit: 1,
     },
     {
-      name: 'DuckDuckGo',
-      limit: 4
+      name: "DuckDuckGo",
+      limit: 4,
     },
   ],
 
@@ -250,8 +134,8 @@ const CONFIG = {
    * Default search suggestions for the specified queries.
    */
   defaultSuggestions: {
-    g: ['g/issues', 'g/pulls', 'gist.github.com'],
-    r: ['r/r/unixporn', 'r/r/startpages', 'r/r/webdev', 'r/r/technology'],
+    g: ["g/issues", "g/pulls", "gist.github.com"],
+    r: ["r/r/unixporn", "r/r/startpages", "r/r/webdev", "r/r/technology"],
   },
 
   /**
@@ -284,18 +168,18 @@ const CONFIG = {
    * The delimiter between a command key and your search query. For example,
    * to search GitHub for potatoes, you'd type "g:potatoes".
    */
-  searchDelimiter: ':',
+  searchDelimiter: ":",
 
   /**
    * The delimiter between a command key and a path. For example, you'd type
    * "r/r/unixporn" to go to "https://reddit.com/r/unixporn".
    */
-  pathDelimiter: '/',
+  pathDelimiter: "/",
 
   /**
    * The delimiter between the hours and minutes on the clock.
    */
-  clockDelimiter: ' ',
+  clockDelimiter: " ",
 
   /**
    * Show a twenty-four-hour clock instead of a twelve-hour clock with AM/PM.
@@ -305,5 +189,5 @@ const CONFIG = {
   /**
    * File extension for icon images
    */
-  iconExtension: 'png'
+  iconExtension: "png",
 };
